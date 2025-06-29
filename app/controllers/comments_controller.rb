1df09203c2517cc92d1492e1496c076a44304288
aym0546 <ciany.f@gmail.com>
2025-06-29 22:47:37 +0900

    Add comment create feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,22 @@ class CommentsController < ApplicationController
     @comment = @task.comments.build
   end
 
+  def create
+    @task = Task.find(params[:task_id])
+    @comment = current_user.comments.build(comment_param)
+    @comment.task = @task
+    if @comment.save
+      redirect_to task_path(@task), notice: 'Added comment!'
+    else
+      flash.now[:error] = 'Comment could not be submitted'
+      render :new
+    end
+  end
+
 
   private
 
   def comment_param
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :task_id)
   end
 end
